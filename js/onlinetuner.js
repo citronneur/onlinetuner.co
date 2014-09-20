ae81c6a4500eee9b7eb97f1739e24dd6cc1db81b
citronneur <citronneur@gmail.com>
2014-09-20 11:20:41 +0200

    understant audio api

--- js/onlinetuner.js
+++ js/onlinetuner.js
@@ -1,19 +1,58 @@
 //onlinetuner.js
 //Namespace declaration
+//Global function
 
 (function () {
 	OnlineTuner = function() {
 		this.VERSION = "1.0";
-			
+		
+		//init audio context
+		if(!this.isHtml5Compatible()) {
+			alert("Not html5 browser compliant");
+			return;
+		}
+		
+		this.audioContext = this.createAudioContext();
 	};
 	
 	OnlineTuner.prototype = {
-		getUserMedia : function(p) {
-			var getUserMedia = 	navigator.getUserMedia || 
-								navigator.webkitGetUserMedia || 
-								navigator.mozGetUserMedia || 
-								navigator.msGetUserMedia;
-			return getUserMedia.call(navigator, p);
+		//Return getUserMedia function for
+		//every browser
+		getUserMediaFunction : function() {
+			return 	navigator.getUserMedia || 
+					navigator.webkitGetUserMedia || 
+					navigator.mozGetUserMedia || 
+					navigator.msGetUserMedia;
+		},
+		
+		//Test html5 API
+		//return false if html5 webRTC API is not available
+		//for browser
+		isHtml5Compatible : function() {
+			return !!this.getUserMediaFunction();
+		},
+		
+		//Call getUsermedia function
+		getUserMedia : function(constraint, successCallback, errorCallback) {
+			return this.getUserMediaFunction().call(navigator, constraint, successCallback, errorCallback);
+		},
+		
+		//Create generic audio context
+		createAudioContext : function() {
+			var AudioContext = window.AudioContext || window.webkitAudioContext;
+			return new AudioContext();
+		},
+		
+		record : function() {
+			var self = this;
+			this.getUserMedia({audio : true}, function(stream) {
+				var analyser = self.audioContext.createAnalyser();
+				analyser.connect(self.audioContext.destination);
+				var input = self.audioContext.createMediaStreamSource(stream);
+				input.connect(analyser);
+			}, function(e) {
+				alert("Error : " + e);
+			});
 		}
 	};
 })();
---
+++ js/recorder.js
@@ -0,0 +1,17 @@
+//recorder.js
+//Use to capture source signal
+
+(function(){
+	var Recorder = function() {
+		
+	};
+	
+	Recorder.prototype = {
+		record : function(input) {
+			
+		},
+	};
+	
+	//Namespace declaration
+	OnlineTuner.Recorder = Recorder;
+})();
