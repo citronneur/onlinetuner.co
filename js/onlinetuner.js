5e4f18234c614b89c606a2f078bfd598442ba04c
citronneur <citronneur@gmail.com>
2014-09-28 22:13:22 +0200

    begin physics of music 2

--- js/onlinetuner.js
+++ js/onlinetuner.js
@@ -28,7 +28,7 @@
 		initUint8Array : function() {
 			//Add max function
 			Uint8Array.prototype.max = function() {
-				Math.max.apply(null, this);
+				return Math.max.apply(null, this);
 			};
 			
 			//Add foreach function
@@ -64,6 +64,18 @@
 		createAudioContext : function() {
 			var AudioContext = window.AudioContext || window.webkitAudioContext;
 			return new AudioContext();
+		},
+		
+		//Return nb half step from A4 note
+		getStepFromFrequency : function(frequency) {
+			return 12 * Math.log2(frequency / 440.0);
+		},
+		
+		getNoteFromFrequency : function(frequency) {
+			var note = ["A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"];
+			
+			var n = Math.round(this.getStepFromFrequency(frequency));
+			return note[12 - (n % 12)] + "" + (4 + Math.round(n / 12));
 		}
 	};
 })();
--- js/tunerview.js
+++ js/tunerview.js
@@ -3,44 +3,15 @@
 
 (function(){
 	
-	var FREQUENCIES = {"e" : 329.6, "B" : 246.9, "G" : 196, "D" : 146.8, "A" : 110, "E" : 82.4 };
-	
-	var TunerView = function(canvas, sampleRate) {
+	var TunerView = function(canvas) {
 		OnlineTuner.View.call(this, canvas);
-		this.sampleRate = sampleRate;
 	};
 	
 	TunerView.prototype = {
-		maxFrequency : function(array) {
-	        var idx = Array.prototype.indexOf.call(array, array.max());
-	        //this compute frequency
-	        return (idx * this.sampleRate / array.length / 2.0);
-		},
-		
-		getNote : function(maxFrequency) {
-			var diffFrequencies = {};
-			for(var i in FREQUENCIES) {
-				diffFrequencies[i] = FREQUENCIES[i] - maxFrequency;
-			}
-			
-			var note = "e";
-			var min = Math.abs(diffFrequencies[note]);
-			for(var i in diffFrequencies) {
-				if(Math.abs(diffFrequencies[i]) < min) {
-					min = Math.abs(diffFrequencies[i]);
-					note = i;
-				}
-			}
-			
-			console.log(note);
-		},
-		
 		// draw a particular array
 		update : function(array) {
 			this.canvasCtx.fillStyle = 'rgb(200, 200, 200)';
 			this.canvasCtx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-			
-			this.getNote(this.maxFrequency(array));
 		}
 	};
 	
